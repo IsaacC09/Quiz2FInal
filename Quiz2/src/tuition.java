6f64f622643f4480d10ae62676ce52aa63a2783b
achouri <achouri@128.4.103.42>
2016-09-26 14:47:31 -0400

    final quiz

--- Quiz2/src/tuition.java
+++ Quiz2/src/tuition.java
@@ -1,38 +1,33 @@
 import java.util.Scanner;
 
-
 public class tuition {
 
 	public static void main(String[] args) {
 
-				Scanner input = new Scanner(System.in);
-				System.out.println("Give a number form 0 to infinity, i.e. 5% = 5 ");
-				
-				System.out.println("Enter tuition amount for your first year at UD. ");
-				double base = input.nextDouble(); // Taking variable for initial tuition cost
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give a number form 0 to infinity, i.e. 5% = 5 ");
 
-				System.out.println("Enter the percentage increase for tuition.");
-				double PER_INC = (input.nextDouble())/100; // Taking variable for percent increase of tuition for second year at UD
-				
-				input.close();
+		System.out.println("Enter tuition amount for your first year at UD. ");
+		double base = input.nextDouble(); // Taking variable for initial tuition
+											// cost
 
-				double[] ar = new double[4]; 
-				 
-				 for (double d : ar) { 
-				 sum += d; 
-				 } 
+		System.out.println("Enter the percentage increase for tuition.");
+		double PER_INC = (input.nextDouble()) / 100; // Taking variable for
+														// percent increase of
+														// tuition for each
+														// year at UD
 
-				for(int x=0; x<4; x++ ){
-					ar.set(0) += base + base*PER_INC;
-					System.out.println(base);
-				}
-				
-			
+		input.close();
 
-				
-				System.out.printf("Total tuition payemnt for 4 years is $%.2f", base);
-		
+		double tuitionSum = base;
+
+		for (int x = 0; x < 3; x++) {
+			base += base * PER_INC;
+			tuitionSum += base;
+			System.out.println(base);
+		}
+
+		System.out.printf("Total tuition payemnt for 4 years is $%.2f", tuitionSum);
 
 	}
-
 }
